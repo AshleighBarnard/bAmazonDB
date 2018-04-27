043f62160298e44491cc93a0e1088c245e57f700
Ashleigh Barnard <ashleighnbarnard@gmail.com>
2018-04-27 14:57:22 -0400

    removing unneccessary things

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,11 +6,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host: 'localhost',
 	port: 3306,
-
-	// Your username
 	user: 'root',
-
-	// Your password
 	password: '',
 	database: 'Bamazon'
 });
